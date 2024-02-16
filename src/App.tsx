dae87a4cdaf69c80e148604d50e87bf94585fe48
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-02-16 23:30:53 +0100

    display how each participant ranked the option

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,60 @@
+import "./App.css";
 import { preferences } from "./preferences.ts";
 import { options } from "./options.ts";
-import "./App.css";
+
+type Rank = number & { __brand: "Rank" };
+
+const rank = (i: number) => i as Rank;
+
+type Pick = {
+  identifier: string;
+  person: string;
+  rank: Rank;
+};
+
+const allPicks: Pick[] = Object.entries(preferences).flatMap(([person, picks]) => {
+  return picks.map((identifier, index) => {
+    return {
+      person,
+      identifier,
+      rank: rank(index),
+    };
+  });
+});
+
+const optionsWithPicks = options.map((option) => {
+  const picks = allPicks.filter(
+    (pick) => pick.identifier === option.identifier,
+  );
+  return {
+    ...option,
+    picks,
+  };
+});
 
 const participants = Object.keys(preferences);
 
 export function App() {
   return (
     <>
-      <ul>
-        {participants.map((participant) => (
-          <li key={participant} className="text-left">
-            {participant}
-          </li>
-        ))}
-      </ul>
+      <ul></ul>
       <table>
+        <thead>
+          <tr>
+            {participants.map((participant) => (
+              <td>{participant}</td>
+            ))}
+            <td>Identifier</td>
+            <td className='text-left pl-4'>Title</td>
+          </tr>
+        </thead>
         <tbody>
-          {options.map((option) => (
+          {optionsWithPicks.map((option) => (
             <tr key={option.identifier}>
+              {participants.map((participant) => {
+                const maybePick = option.picks.find(pick => pick.person === participant);
+                return <td>{maybePick? maybePick.rank: ''}</td>;
+              })}
               <td className="items-start">{option.identifier}</td>
               <td className="text-left pl-4">{option.name}</td>
             </tr>
