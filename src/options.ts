2e5ac39981d82a48556d233a1df537784d9c2905
Thomas Heilbronner <thomas.heilbronner@gmail.com>
2024-02-17 11:34:27 +0100

    read keywords and description

--- src/options.ts
+++ src/options.ts
@@ -4,6 +4,8 @@ import optionsJson from "../inputs/options.json";
 const optionSchema = z.object({
   identifier: z.string(),
   name: z.string(),
+  keywords: z.array(z.string()),
+  description: z.string(),
 });
 export type Option = z.infer<typeof optionSchema>;
 const optionsSchema = z.array(optionSchema);
